186f067d2a9823ee1d44fc605fa7d8da30cc8663
Galen Montague <gsmontaguefamily@yahoo.com>
2019-03-01 16:07:03 -0700

    Nav hover complete

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,23 +9,37 @@ export default class NavigationContainer extends Component {
   
     render() {
         return (
-            <div>
-                <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/" activeClassName="nav-link-active">
+                            Home
+                        </NavLink>
+                    </div>
 
-                <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+                    </div>
 
-                <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                    <div className="nav-link-wrapper">    
+                        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                    </div>
+                    
+                    <div className="nav-link-wrapper">         
+                        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                    </div>
+                        {/* NavLink enables the active class functionality */}
 
-                <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                    {/* NavLink enables the active class functionality */}
 
+                    {/* <a href="/">Wrong Home</a> */}
+                        {/* This will reload the entire page. This goes around how react is supposed to work! */}
+                        {/* can use a tags if navigating to outside sites (but not internal component) */}
 
-                {/* <a href="/">Wrong Home</a> */}
-                    {/* This will reload the entire page. This goes around how react is supposed to work! */}
-                    {/* can use a tags if navigating to outside sites (but not internal component) */}
+                    {false ? <button>Add Blog</button> : null}
+                    {/* // ternary operator */}
+                </div>
 
-                {false ? <button>Add Blog</button> : null}
-                {/* // ternary operator */}
+                <div className="right-side">GALEN MONTAGUE</div>
             </div>
         );
     }
